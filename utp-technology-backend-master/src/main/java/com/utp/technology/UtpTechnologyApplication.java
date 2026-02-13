46e86523343cb95eb16e5faa30506858dd10379a
Roberto Agustín Mejía Collazos <robertomejiacollazos@gmail.com>
2026-02-12 22:12:39 -0500

    fix: Remove MySQL dependencies and disable DataSource auto-configuration for Firestore-only mode

--- utp-technology-backend-master/src/main/java/com/utp/technology/UtpTechnologyApplication.java
+++ utp-technology-backend-master/src/main/java/com/utp/technology/UtpTechnologyApplication.java
@@ -3,7 +3,9 @@ package com.utp.technology;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class UtpTechnologyApplication {
 
   public static void main(String[] args) {
